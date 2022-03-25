b5cf17b61de788581cb429aecd6f47cfdb9bc989
Aleksandra <aleksandra.latk@gmail.com>
2022-03-25 12:41:08 +0100

    Add InstrukcjaWarunkowaIf

---
+++ src/InstrukcjaWarunkowaIf.java
@@ -0,0 +1,15 @@
+public class InstrukcjaWarunkowaIf {
+
+    public static void main(String[] args) {
+        float dzielna = 4.0F;
+        float dzielnik = 0.0F;
+
+        if (dzielnik != 0) {
+            System.out.println("Wynik dzielania to: " + dzielna/ dzielnik);
+        } else {
+            System.out.println("Proszę podaj inną liczbę różną od zera");
+        }
+
+
+    }
+}
